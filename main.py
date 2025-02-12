c6012b02a30f17fa6cdaf5d9029f620b7ce16c98
Devansh Singh <devanshamity@gmail.com>
2025-02-12 16:44:23 +0530

    Add regex query support

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import argparse
 import mmap
+import re
 import time
 
 from rich.console import Console
@@ -9,10 +10,18 @@ from rich.text import Text
 console = Console()
 
 
+def format_line(line: str, start: int, end: int) -> None:
+    text = Text.assemble(
+        line[:start], (line[start:end], "bold red"), line[end:])
+    console.print(text)
+
+
 # TODO:
 # Figure out how to use multiprocessing with mmap
 # Use file chunks using seek
-def read_file(filename: str, query: str | None = None):
+def read_file(
+    filename: str, query: str | None = None, regex: str | None = None
+) -> None:
     with open(filename, "r+b") as infile:
         with mmap.mmap(infile.fileno(), length=0, access=mmap.ACCESS_READ) as map:
             line_count = 0
@@ -21,21 +30,23 @@ def read_file(filename: str, query: str | None = None):
                 if query and (query in line):
                     pos_start = line.find(query)
                     pos_end = pos_start + len(query)
-                    text = Text.assemble(
-                        line[:pos_start],
-                        (line[pos_start:pos_end], "bold red"),
-                        line[pos_end:],
-                    )
-                    console.print(text)
+                    format_line(line, pos_start, pos_end)
+                if regex and (match := re.search(regex, line)):
+                    format_line(line, match.start(), match.end())
                 line_count += 1
             time_end = time.time()
             console.print(
                 f"read {line_count} lines in {time_end - time_start:.4f}s")
 
 
+# TODO:
+# Add optional argument for storing results in a file
+# Search in multiple log files (multiprocessing)
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("file", help="name of log file", type=str)
     parser.add_argument("-q", "--query", help="search query", type=str)
+    parser.add_argument(
+        "-r", "--regex", help="search query as regex", type=str)
     args = parser.parse_args()
-    read_file(args.file, args.query)
+    read_file(args.file, args.query, args.regex)
